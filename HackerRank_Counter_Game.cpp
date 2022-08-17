efe362ad435232e42fe1945814ad7a463949f7f9
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-08-17 23:03:00 +0530

    #BIT MANIPULATION

---
+++ HackerRank_Counter_Game.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'counterGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+
+string counterGame(long n) {
+    string Richard = "Richard";
+    string Louise = "Louise";
+    if(n==1){
+        return Richard;
+    }
+    int counter=0;
+    while(n!=1){
+        if((n&(n-1))==0){
+            n=n>>1;
+        }
+        else{
+            int k = log2(n)+1;
+            n = n - pow(2,k-1);
+        }
+        counter++;
+    }
+    return ( (counter-1)%2 == 0)? Louise : Richard;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        string result = counterGame(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
