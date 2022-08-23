01fca79b8e7b56aa6928e3c952e3aabab6a03c53
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-08-23 21:38:18 +0530

    Create 1409_A_Yet Another Two Integers Problem.cpp
    #_CODEFORCES

---
+++ 1409_A_Yet Another Two Integers Problem.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+#include<cmath>
+#include<string>
+#include <iterator>
+#include <sstream>
+#include<stack>
+
+#define f(i,a,b) for(ll i = a;i < b;i++)
+
+typedef long long ll;
+
+using namespace std;
+
+const ll INF = 1e9 + 7;
+
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    
+    cout<<(abs(b-a)+9)/10<<"\n";
+	
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	
+	while(t--)
+    {
+        solve();
+    }
+	
+	return 0;
+    
+}
