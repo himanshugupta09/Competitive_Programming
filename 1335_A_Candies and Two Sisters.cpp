1925fdc9cc37f058430b1ed22687b1ad9f7c262c
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-08-24 21:59:25 +0530

    # CODEFORCES
    # CODEFORCES

---
+++ 1335_A_Candies and Two Sisters.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+#include<cmath>
+#include<string>
+#include <iterator>
+#include <sstream>
+#include<stack>
+
+#define f(i,a,b) for(ll i = a;i < b;i++)
+
+typedef long long ll;
+
+using namespace std;
+
+const ll INF = 1e9 + 7;
+
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    
+    cout<<abs(((n-1)/2))<<"\n";
+	
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	
+	while(t--)
+    {
+        solve();
+    }
+	
+	return 0;
+    
+}
