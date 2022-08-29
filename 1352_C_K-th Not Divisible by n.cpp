a305da7d6387ff98fab2db9991abf0f512b858e5
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-08-29 23:25:26 +0530

    ok
    # CODEFORCES 
    # BINARY SEARCH MATHS 1200+

---
+++ 1352_C_K-th Not Divisible by n.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <map>
+#include <set>
+#include <cmath>
+#include <deque>
+#include <random>
+#include <iomanip>
+#include <chrono>
+#include <bitset>
+#include <queue>
+#include <complex>
+#include <functional>
+
+#define f(i,a,b) for(ll i = a;i < b;i++)
+
+typedef long long ll;
+
+using namespace std;
+
+const ll INF = 1e9 + 7;
+
+ll md = 998244353;
+int const maxi = 2000;
+int a[maxi][maxi];
+
+
+void solve()
+{
+  
+    ll n,k;
+    cin>>n>>k;
+    
+    cout<<(k + ((k-1)/(n-1)))<<"\n";
+    
+    
+    
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	
+	while(t--)
+    {
+        solve();
+    }    
+	
+	return 0;
+    
+}
