4b19fc589bf0e2e335b77f264edfa70e407787ba
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-08-31 11:37:15 +0530

    ok
    # CODEFORCES

---
+++ 1722_B_ColourBlindness.py
@@ -0,0 +1,27 @@
+def solve():
+	_ = int(input())
+	a = input()
+	b = input()
+	
+	c = b.replace('B','G')
+	d = a.replace('B','G')
+	
+	if c == d:
+		print("yes")
+	else:
+		print("no")	
+	
+	
+	
+ 
+						
+				
+	
+ 
+ 
+ 
+t = int(input())
+while t != 0:
+	solve()
+	t -= 1		
+	
