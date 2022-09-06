04bbb31bd3870a8ed4bdaa5b3b6d908950f7cd2f
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-09-06 13:16:13 +0530

    Create 1721_A_image.cpp
    # CODEFORCES

---
+++ CODEFORCES/1721_A_image.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <map>
+#include <set>
+#include <cmath>
+#include <deque>
+#include <random>
+#include <iomanip>
+#include <chrono>
+#include <bitset>
+#include <queue>
+#include <complex>
+#include <functional>
+
+#define f(i,a,b) for(ll i = a;i < b;i++)
+
+typedef long long ll;
+
+using namespace std;
+
+const ll INF = 1e9 + 7;
+
+ll md = 998244353;
+int const maxi = 2000;
+int a[maxi][maxi];
+
+
+ll gcd(ll u, ll v) {
+  ll shift;
+  if (u == 0)
+    return v;
+  if (v == 0)
+    return u;
+  shift = __builtin_ctz(u | v);
+  u >>= __builtin_ctz(u);
+  do {
+    ll m;
+    v >>= __builtin_ctz(v);
+    v -= u;
+    m = (int)v >> 31;
+    u += v & m;
+    v = (v + m) ^ m;
+  } while (v != 0);
+  return u << shift;
+}
+
+
+ll lcm(ll v1, ll v2) {
+    ll divisor = (v1 > v2 ?gcd(v1, v2) : gcd(v2, v1));
+    return v1 * v2 / divisor;
+  }
+
+void solve()
+{
+  string s1,s2;
+  cin>>s1;
+  cin>>s2;
+  
+  s1+=s2;
+  
+  cout<<set<char>(s1.begin(),s1.end()).size()-1<<"\n";
+  
+  
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	
+	while(t--)
+    {
+        solve();
+    }    
+	
+	return 0;
+    
+}
