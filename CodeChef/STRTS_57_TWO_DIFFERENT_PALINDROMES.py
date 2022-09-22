3d78c63aa1fb6b2c926282c13d2e6de9d7347f11
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-09-22 23:23:40 +0530

    Create STRTS_57_TWO_DIFFERENT_PALINDROMES.py

---
+++ CodeChef/STRTS_57_TWO_DIFFERENT_PALINDROMES.py
@@ -0,0 +1,18 @@
+#from collections import Counter
+
+def CC():
+  f1,f2 = map(int,input().split())
+  print("No") if f1 == 1 or f2 == 1 else print("No") if f1%2 == 1 and f2%2 == 1 else print("Yes") 
+  
+ 
+      
+  
+  
+
+
+t = int(input())
+while t != 0:
+  CC()
+  t -=1
+    
+        
