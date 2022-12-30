da8b574ab558a253331096b970913d8c34f1f5cd
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-12-30 20:22:36 +0530

    Create Watson-asks-Does-Permuatation-Exist.cpp

---
+++ CodeChef/Watson-asks-Does-Permuatation-Exist.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+
+#define f(i,a,b) for(ll i = a;i < b;i++)
+
+
+
+typedef long long ll;
+
+using namespace std;
+
+/*
+const ll INF = 1e9 + 7;
+
+ll md = 998244353;
+int const maxi = 2000;
+int a[maxi][maxi];
+*/
+/*
+ll gcd(ll u, ll v) {
+  ll shift;
+  if (u == 0)
+    return v;
+  if (v == 0)
+    return u;
+  shift = __builtin_ctz(u | v);
+  u >>= __builtin_ctz(u);
+  do {
+    ll m;
+    v >>= __builtin_ctz(v);
+    v -= u;
+    m = (int)v >> 31;
+    u += v & m;
+    v = (v + m) ^ m;
+  } while (v != 0);
+  return u << shift;
+}
+
+
+ll lcm(ll v1, ll v2) {
+    ll divisor = (v1 > v2 ?gcd(v1, v2) : gcd(v2, v1));
+    return v1 * v2 / divisor;
+  }
+*/
+
+/*bool contains(vector<ll> vec, ll elem)
+{
+    bool result = false;
+    if( find(vec.begin(), vec.end(), elem) != vec.end() )
+    {
+        result = true;
+    }
+    return result;
+}
+*/
+
+/*
+bool cmp(pair<int, int>& a,
+         pair<int, int>& b)
+{
+    return a.second < b.second;
+}
+
+void sort(map<int, int>& M)
+{
+  
+    
+    vector<pair<int, int> > A;
+  
+    
+    for (auto& it : M) {
+        A.push_back(it);
+    }
+  
+    
+    sort(A.begin(), A.end(), cmp);
+  
+    cout<<M.size()<<"\n";
+    
+    
+    for (auto& it : A) {
+  
+        cout << it.first<<" ";
+    }
+}
+*/
+
+/*
+void dfs(int cur,vector<string> &S,vector<string> &T,int remain,string ans){
+	
+	if(remain<0)return;
+	
+	if(cur == S.size()){
+		if(ans.size()>=3&&!binary_search(T.begin(),T.end(),ans)){
+			cout<<ans<<endl;
+			exit(0);
+		}
+		return;
+	}
+	
+	if(ans.size()>0 && ans.back()!='_'){
+		dfs(cur,S,T,remain,ans + "_");
+	}
+	else{
+		dfs(cur+1,S,T,remain,ans + S[cur]);
+		if(ans.size()>0)dfs(cur,S,T,remain-1,ans + "_");
+	}
+	
+}
+*/
+
+
+
+
+
+void solve()
+{
+  
+ 
+  int n;
+  cin >> n;
+  
+  vector<int>v(n);
+  
+  for(int i=0;i<n;i++)
+  {
+      cin >> v[i];
+      
+  }
+  
+  for(int i=0;i<n-1;i++)
+  {
+      if(v[i+1]-v[i] > 1)
+      {
+          cout << "NO\n";
+          return;
+      }
+  }
+  cout << "YES\n";
+  return;
+  
+}
+  
+
+  
+  
+ 
+ // cout<<"\n";
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	
+while(t--)
+    {
+        solve();
+    }    
+	
+	return 0;
+    
+}
