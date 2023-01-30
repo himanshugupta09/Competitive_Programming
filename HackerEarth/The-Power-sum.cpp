9bb1780204e802bfc20514019335c7afdbaadc34
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-01-30 16:08:48 +0530

    Create The-Power-sum.cpp

---
+++ HackerEarth/The-Power-sum.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'powerSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER X
+ *  2. INTEGER N
+ */
+
+int recurSolve(int X,int N,int n)
+{
+    int pow_num = pow(n,N);
+    if(pow_num == X)
+    {
+        return 1;
+    }
+    else if(pow_num > X)
+    {
+        return 0;
+    }
+    
+       return recurSolve(X,N,n+1) + recurSolve(X-pow_num,N,n+1);
+    
+}
+
+
+int powerSum(int X, int N) {
+    
+    
+    return recurSolve(X,N,1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string X_temp;
+    getline(cin, X_temp);
+
+    int X = stoi(ltrim(rtrim(X_temp)));
+
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    int result = powerSum(X, N);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
